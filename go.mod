a5081bcd75bc29332dd67ee130983d648d1ee223
Anders Markendahl <andersmarkendahl@gmail.com>
2020-02-09 08:07:36 +0100

    src: Set initial velocity

--- go.mod
+++ go.mod
@@ -4,5 +4,6 @@ go 1.13
 
 require (
 	github.com/Aoana/ball-sim-go v0.0.0-20200202174625-da99c1496a6d
+	github.com/atedja/go-vector v0.0.0-20170318030839-a887ff7bead7
 	github.com/hajimehoshi/ebiten v1.11.0-alpha.2.0.20200113095530-6eb05a0203d7
 )
--- internal/pkg/stars/stars.go
+++ internal/pkg/stars/stars.go
@@ -2,8 +2,8 @@ package stars
 
 import (
 	"fmt"
-	"github.com/Aoana/ball-sim-go/pkg/mathutil"
 	"github.com/Aoana/ball-sim-go/pkg/objects"
+	"github.com/atedja/go-vector"
 	"image/color"
 )
 
@@ -33,14 +33,38 @@ func init() {
 // Fixed starting position and velocity is random
 func StartValues(r int) error {
 
+	T := vector.NewWithValues([]float64{float64(W / 2), float64(H / 2)})
+
 	for i := -r; i <= r; i++ {
 		for j := -r; j <= r; j++ {
 			if i*i+j*j <= r*r {
-				// Random starting velocity
-				vx0, _ := mathutil.RandInRange(-0.5, 0.5)
-				vy0, _ := mathutil.RandInRange(-0.5, 0.5)
+
+				// Logical starting position
+				X := vector.NewWithValues([]float64{float64(i), float64(j)})
+
+				// Velocity vector
+				var V vector.Vector
+
+				// Velocity perpendicular to circle
+				if X[0] == 0 && X[1] == 0 {
+					continue
+				} else if X[0] == 0 {
+					V = vector.NewWithValues([]float64{X[1], 0.0})
+				} else if X[0] > 0 {
+					V = vector.NewWithValues([]float64{X[1] / X[0], -X[0]})
+				} else {
+					V = vector.NewWithValues([]float64{-X[1] / X[0], -X[0]})
+				}
+
+				// Velocity vector with fixed length
+				VS := vector.Unit(V)
+				VS.Scale(0.5)
+
+				// Translate position to middle of screen
+				XT := vector.Add(X, T)
+
 				// Construct objects
-				s, err := objects.New(float64(W/2+i), float64(H/2+j), vx0, vy0)
+				s, err := objects.New(XT[0], XT[1], VS[0], VS[1])
 				if err != nil {
 					return err
 				}
